7614a6fa0a7a0106fdc986edf9c1927234e0d8b2
Toshiyuki023Hori <World.88Toshi@gmail.com>
2021-11-12 09:09:23 +0900

    Solve_Atcoder_Market

--- Nov/20211110.py
+++ Nov/20211110.py
@@ -10,19 +10,22 @@ exit_array = []
 
 for i in range(N):
     one_raw = list(map(int, input().split()))
-    enter = one_raw[0]
-    exit = one_raw[1]
-    enter_array.append(enter)
-    exit_array.append(exit)
+    enter_array.append(one_raw[0])
+    exit_array.append(one_raw[1])
     item_places_array.append(one_raw)
-enter_array.sort
-exit_array.sort
+enter_array.sort()
+exit_array.sort()
+print(enter_array)
+print(exit_array)
 # 大きさが真ん中のものが入り口と出口に決定される
 enter_num = enter_array[N//2]
 exit_num = exit_array[N//2]
 
 # 各列で差を求めてやればいい
+# 段階としては3段階
+# ENTER-A | A-B | B-EXIT
+# 以上3段階の和
 total_seconds = 0
 for item_place in item_places_array:
-    total_seconds += abs(enter_num - item_place[0]) + abs(exit_num - item_place[1]) + abs(item_place[0] - item_place[1])
+    total_seconds += abs(enter_num - item_place[0]) + abs(item_place[0] - item_place[1]) + abs(item_place[1] - exit_num)
 print(total_seconds)
