de02e20d2b17317dce1eb77c8d056e085706eae9
Toshiyuki023Hori <World.88Toshi@gmail.com>
2022-01-25 23:33:41 +0900

    abc201_c

--- 2022/Jan/20220120.py
+++ 2022/Jan/20220120.py
@@ -1,34 +1,35 @@
 # https://atcoder.jp/contests/abc201/tasks/abc201_c
-S = input()
-ok_num = []
-ng_num = []
-
-count = 0
-
-for i, s in enumerate(S):
-    if s == 'o':
-        ok_num.append(str(i))
-    if s == 'x':
-        ng_num.append(str(i))
-
-
-def is_num_ok(num: str) -> bool:
-    for o in ok_num:
-        if o == '0' and len(num) <= 3:
-            continue
-        if not(o in num):
-            return False
-    for n in ng_num:
-        if n == '0' and len(num) <= 3:
-            return False
-        if n in num:
-            return False
+# これは解けなかった問題です
+# 解説見ながら自分でコード書いた感じ
+# 4桁の暗証番号なので、10**4 = 10,000通りなので十分全探索できる
+def judge(s, must_array, error_array) :
+    for o in must_array:
+        # oはsに確実に含まれています
+        if o not in s:
+           return False
+    for x in error_array:
+        # xはsに確実に含まれていません
+        if x in s:
+           return False
     return True
 
+S = input()
 
-for j in range(10000):
-    str_j = str(j)
-    if is_num_ok(str_j):
-        count += 1
+# 絶対に入っているリストと絶対に含まれていないリストを作る
+must_nums = []
+error_nums = []
 
-print(count)
+for idx, char in enumerate(S):
+    if char == 'o' :
+        must_nums.append(str(idx))
+    elif char == 'x' :
+        error_nums.append(str(idx))
+
+ans = 0
+# 0000 ~ 9999を生成
+for i in range(10000):
+    s = str(i).zfill(4)
+    if judge(s, must_nums, error_nums) :
+        ans += 1
+print(ans)
+
